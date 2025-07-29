c415d147179b6ba14ad0231b8d26a261cd13ba10
Manindra Singh <manindra23@yahoo.co.in>
2025-07-29 19:24:54 +0530

    implementing model - train, evaluate, generate report

--- projects/building_machine_learning_pipeline_from_scratch/ml_pipeline_tutorial/main.py
+++ projects/building_machine_learning_pipeline_from_scratch/ml_pipeline_tutorial/main.py
@@ -1,4 +1,10 @@
+import pandas as pd
+
+from numpy.random import default_rng
+from omegaconf import OmegaConf
+
 from ml_pipeline.datasets.iris import IrisDataset
+from ml_pipeline.models.iris_classifier import IrisClassifier
 
 if __name__ == "__main__":
     print("Instantiating IrisDataset...")
@@ -23,3 +29,24 @@ if __name__ == "__main__":
     #)
     print("Saving feature engineered data to disk...")
     dataset.save("artifacts", "feature_engineered")
+
+
+    # generate dummy data
+    num_rows = 10
+    num_columns = 4
+    rng = default_rng()
+    X = pd.DataFrame(rng.random(size=(num_rows, num_columns)))
+    y = pd.Series(rng.random(size=num_rows))
+
+    # classifier and training parameters
+    model_params = OmegaConf.create({"penalty": "l2"})
+    training_params = OmegaConf.create({"test_split": 0.3})
+
+    # instantiate model object and train the model
+    model = IrisClassifier(model_params, training_params, "artifacts")
+    idx_train, idx_test = model.train(X, y)
+    # save the trained model
+    model.save()
+
+
+
